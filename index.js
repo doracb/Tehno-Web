0d4914d90a001795bb989d25ccd79c743fa25cf8
Dora Condurache-Bota <doracb2@gmail.com>
2025-10-03 15:10:49 +0300

    am adaugat restul fisierelor

--- index.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-
-// create a server object:
-http.createServer(function (req, res) {
-  res.write("Hello from VS Code!"); // write a response
-  res.end(); // end the response
-}).listen(8080); // server listens on port 8080
