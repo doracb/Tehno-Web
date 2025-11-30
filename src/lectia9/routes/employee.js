36586f5f9176f330dbb063d4d8db4f3910192cfd
Dora Condurache-Bota <doracb2@gmail.com>
2025-11-30 23:46:44 +0200

    lectia9

---
+++ src/lectia9/routes/employee.js
@@ -0,0 +1,75 @@
+const { Op } = require("sequelize"); //operatorii din sequelize
+const Employee = require("../models/employee");
+
+const router = require("express").Router();
+
+router
+  .route("/employees")
+  .get(async (req, res) => {
+
+    const { simplified, minSalary, name, sortBy, order } = req.query;
+    const where = {};
+
+    try {
+      if (minSalary) {
+        where.salary = { [Op.gt]: minSalary }
+      }
+
+      if (name) {
+        where.firstName = { [Op.like]: `%${name}%` }
+      }
+
+      //const employees = await Employee.findAll({ where: where, attributes: ['firstName', 'lastName'] });
+      const employees = await Employee.findAll({
+        where: where,
+        attributes: simplified ? { exclude: 'id' } : undefined,
+        order: sortBy ? [[sortBy, (order?.toUpperCase() === "DESC" ? "DESC" : "ASC")]] : undefined
+      });
+
+      return res.status(200).json(employees);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const newEmployee = await Employee.create(req.body);
+      return res.status(200).json(newEmployee);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+router
+  .route("/employees/:id")
+  .get(async (req, res) => {
+    const employee = await Employee.findByPk(req.params.id);
+    if (employee) {
+      return res.status(200).json(employee);
+    }
+    else {
+      return res.status(404).json({ error: `Employee with id ${req.params.id} not found.` })
+    }
+  })
+  .put(async (req, res) => {
+    const employee = await Employee.findByPk(req.params.id);
+    if (employee) {
+      const updateEmployee = await employee.update(req.body);
+      return res.status(200).json(updateEmployee);
+    }
+    else {
+      return res.status(404).json({ error: `Employee with id ${req.params.id} not found.` })
+    }
+  })
+  .delete(async (req, res) => {
+    const employee = await Employee.findByPk(req.params.id);
+    if (employee) {
+      await employee.destroy();
+      return res.status(200).json('Employee deleted successfully.');
+    }
+    else {
+      return res.status(404).json({ error: `Employee with id ${req.params.id} not found.` })
+    }
+  });
+
+module.exports = router;
